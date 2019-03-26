595e845e427ca3378af0aa046ede5db433cbe075
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-03-26 09:38:29 -0300

    boot file

--- boot/datetimepicker.js
+++ boot/datetimepicker.js
@@ -1,4 +1,4 @@
-import QDatetimeDicker from '@quasar/quasar-app-extension-datetimepicker/component/QDatetimeDicker'
+import QDatetimeDicker from '@quasar/quasar-app-extension-datetimepicker/component/QDatetimePicker'
 
 export default async ({ Vue }) => {
   Vue.component('q-datetime-picker', QDatetimeDicker)
