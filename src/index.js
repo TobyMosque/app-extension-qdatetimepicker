c1b022c82c50ebcdf6de574be19059a8e0df1539
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-04-16 12:56:01 -0300

    compatiblity check

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ const extendDatetimePicker = function (conf, ctx) {
 }
 
 module.exports = function (api, conf) {
-  api.compatibleWith('@quasar/app', '^1.0.0-beta.17')
+  var compatibleWith = api.compatibleWith || function (package, version) {
+    return api.compatibleWithQuasarApp(version)
+  }
+  compatibleWith('@quasar/app', '^1.0.0-beta.17')
   api.registerDescribeApi('QDatetimePicker', '../component/QDatetimePicker.json')
   api.extendQuasarConf(extendDatetimePicker)
 }
--- src/install.js
+++ src/install.js
@@ -1,3 +1,6 @@
 module.exports = function (api) {
-  api.compatibleWith('@quasar/app', '^1.0.0-beta.17')
+  var compatibleWith = api.compatibleWith || function (package, version) {
+    return api.compatibleWithQuasarApp(version)
+  }
+  compatibleWith('@quasar/app', '^1.0.0-beta.17')
 }
