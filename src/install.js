b9ba26f2bf16203c7a0783fede06bd5efa3f853f
Tobias de Abreu Mesquita <tobias.mesquita@gmail.com>
2019-05-14 17:56:19 -0300

    updating quasar version

--- src/install.js
+++ src/install.js
@@ -2,5 +2,5 @@ module.exports = function (api) {
   var compatibleWith = api.compatibleWith || function (_, version) {
     return api.compatibleWithQuasarApp(version)
   }
-  compatibleWith('@quasar/app', '^1.0.0-beta.22')
+  compatibleWith('@quasar/app', '^1.0.0-beta.23')
 }
