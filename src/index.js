a012c0a324d61d90184eaa572e3a77936175b168
Tobias <Tobias@DESKTOP-CRCGFNT>
2020-06-19 11:29:30 -0300

    app 2.0 fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ module.exports = function (api) {
   var compatibleWith = api.compatibleWith || function (_, version) {
     return api.compatibleWithQuasarApp(version)
   }
-  compatibleWith('@quasar/app', '^1.0.0-beta.23')
+  // compatibleWith('@quasar/app', '^1.0.0-beta.23')
 
   api.registerDescribeApi('QDatetimePicker', './component/QDatetimePicker.json')
   api.extendQuasarConf(extendDatetimePicker)
