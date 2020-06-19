9b770521f4b0bd2d6af646a999b3bf8441e43768
Tobias <Tobias@DESKTOP-CRCGFNT>
2020-06-19 11:33:00 -0300

    app 2 fix

--- src/install.js
+++ src/install.js
@@ -2,5 +2,5 @@ module.exports = function (api) {
   var compatibleWith = api.compatibleWith || function (_, version) {
     return api.compatibleWithQuasarApp(version)
   }
-  compatibleWith('@quasar/app', '^1.0.0-beta.23')
+  // compatibleWith('@quasar/app', '^1.0.0-beta.23')
 }
